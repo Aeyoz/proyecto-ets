bc99da575ad83f2f0636dbad800dac11cf485bcd
marlonfrz <marlonfh2b1112@gmail.com>
2023-05-12 15:22:14 +0100

    primeros cambios en el codigo

---
+++ source/person.py
@@ -0,0 +1,55 @@
+class Person:
+    DNI_VALID_CHARS = "TRWAGMYFPDXBNJZSQVHLCKE"
+    code = 0
+    def __init__(self, dni:str,  name:str, surnames: str, address: str, phone: str, email: str, birth_date: str):
+        self.code = Person.automatic_code()              # No
+        self.dni = dni                 # No
+        self.birth_date = birth_date   # No
+        self.name = name
+        self.surnames = surnames   
+        self.address = address
+        self.phone = phone
+        self.email = email
+
+    def name_change(self, new_name):
+        self.name = new_name
+
+    def surname_change(self, new_surname):
+        self.surname = new_surname
+
+    def address_change(self, new_address):
+        self.address = new_address
+
+    def phone_change(self, new_phone):
+        self.phone = new_phone
+
+    def email_change(self, new_email):
+        self.email = new_email
+    
+    def __str__(self):
+        return f"{self.name} with code: {self.code}"
+
+    @staticmethod
+    def automatic_code():
+        Person.code += 1
+        new_code = Person.code
+        return new_code
+
+pepe = Person("4337245g", "Pepe", "Gonzalez", "Calle 123", "145223589", "anpch@example.com", "1990-01-01")
+print(pepe)
+pepe2 = Person("4337245g", "Pepe", "Gonzalez", "Calle 123", "145223589", "anpch@example.com", "1990-01-01")
+print(pepe2)
+
+
+def check_dni(method):
+    def wrapper(self, *args, **kwargs):
+        if Person.DNI_VALID_CHARS[self.dni % 23] != self.dni[-1]:
+            raise InvalidDNI(self.dni)
+    return wrapper
+
+class InvalidDNI(Exception):
+    def __str__(self, message = "Invalid DNI"):
+        super().__message__ = message
+
+    def __str__(self):
+        return f"{self.dni} -> {self.message}"
